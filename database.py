a5c517fd7fda2dd730c31b3f2332e57fe64f784f
Dain <dain_atreides@outlook.com>
2025-04-13 11:26:51 +0500

    Update code

--- database.py
+++ database.py
@@ -2,19 +2,42 @@ import sqlite3
 
 # Функция для создания/подключения базы данных
 def connect_db():
-    conn = sqlite3.connect('todo.db')
-    cursor = conn.cursor()
-    pass
+    db = sqlite3.connect('todo.db')
+    cursor = db.cursor()
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS 
+            tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            status BOOLEAN DEFAULT 0
+            )
+        '''
+    )
+    db.commit()
+    return db, cursor
 
-# CRUD
-def create_task():
-    pass
+# CRUD (Create Read Update Delete)
 
+# Функция для создания новой задачи
+def create_task(title: str, status=False):
+    db, cursor = connect_db()
+    cursor.execute(
+        '''
+        INSERT INTO tasks (title, status) VALUES (?, ?)
+        ''', (title, int(status))
+    )
+    db.commit()
+    db.close()
+
+#
 def get_tasks():
     pass
 
+#
 def update_task():
     pass
 
+#
 def delete_task():
     pass
